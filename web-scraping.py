b96818a34280f73b93b0d6b0594a3a2d269aee9e
EdnaEspejoUOC <72635756+EdnaEspejoUOC@users.noreply.github.com>
2020-10-14 21:28:47 -0500

    Update web-scraping.py
    Se incluye la función para extraer los campos de la URLs y la construcción del archivo datacourses.csv

--- web-scraping.py
+++ web-scraping.py
@@ -2,11 +2,12 @@
 """
 Created on Sun Oct 11 12:06:04 2020
 
-@author: LuisL
+@author: LuisL y EdnaE
 """
 
 
 import requests
+import pandas as panda
 from bs4 import BeautifulSoup as bs
 
 
@@ -67,9 +68,71 @@ Process url with specific topic
 """
 def processHtml():
     
+    listcourses = []
     url = "https://www.coursera.org/sitemap.xml"
     topic = "courses"
-    print(processUrl(url, topic))
-    
+    listcourses = (processUrl(url, topic))    
+    findClass(listcourses)
 
+    
+"""
+Extract Dataset
+""" 
+def findClass(listcourses):
+    
+    CourseName = []
+    CourseCategories = []
+    CourseCategory = []
+    CourseSubcategory = []
+    Enrolled = []
+    Difficulty = []
+    Rating = []
+    RatingCount = []
+    print('inicio del proceso')
+    
+    for Course in listcourses:
+        webpage = requests.get(Course)
+        soup = bs(webpage.content, 'html.parser')
+        if((soup.find('h1', class_='banner-title m-b-0 banner-title-without--subtitle')) != None):
+            CourseName.append( soup.find('h1', class_='banner-title m-b-0 banner-title-without--subtitle').get_text() )
+        else: 
+            CourseName.append('')
+        CourseCategories = soup.find_all('a', class_='_172v19u6 color-white font-weight-bold')
+        try:
+            CourseCategory.append( CourseCategories[1].get_text() )
+        except IndexError:
+            CourseCategory.append('No Category')
+        try:
+            CourseSubcategory.append( CourseCategories[2].get_text() )
+        except IndexError:
+            CourseSubcategory.append('No Subcategory')
+        if((soup.find('div',class_='rc-ProductMetrics')) != None):
+            Enrolled.append( soup.find('div',class_='rc-ProductMetrics').find_all('span')[1].get_text() )
+        else: 
+            Enrolled.append('0')
+        if((soup.find('div', class_='_16ni8zai m-b-0 m-t-1s')) != None):
+            Difficulty.append( soup.find('div', class_='_16ni8zai m-b-0 m-t-1s').get_text() )
+        else: 
+            Difficulty.append('0')
+        if((soup.find('div', class_='rc-ReviewsOverview__totals__rating')) != None):
+            Rating.append( soup.find('div', class_='rc-ReviewsOverview__totals__rating').get_text() )
+        else: 
+            Rating.append('')
+        if((soup.find('div', class_='_wmgtrl9 color-white ratings-count-expertise-style')) != None):
+            RatingCount.append( soup.find('div', class_='_wmgtrl9 color-white ratings-count-expertise-style').find('span').get_text() )
+        else: 
+            RatingCount.append('')
+        
+    dataCourses = panda.DataFrame({'course_name': CourseName,
+                          'course_category': CourseCategory,
+                          'course_subcategory': CourseSubcategory,
+                          'students_enrolled':Enrolled,
+                          'course_difficulty':Difficulty,
+                          'course_rating':Rating,
+                          'course_rating_count':RatingCount})
+    dataCourses.to_csv('datacourses.csv')
+    print('archivo generado')
+
+        
+        
 processHtml()
